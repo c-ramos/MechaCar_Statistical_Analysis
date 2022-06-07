bd4d2bc6b6266606b6881d0619b9e33786e9a241
c-ramos <carolinaram@me.com>
2022-06-07 18:27:57 -0400

    deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -8,3 +8,13 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 # find the p-value and r-squared
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar))
 
+library(tidyverse)
+
+#Read CSV file
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#Write an Rscript that creates total_summary DF using summarize() function to get med, variance, and STD
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep') 
+
+#write script that creates a lot_summary DF using groupby() and summarize()
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep')
