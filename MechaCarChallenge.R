cd156b340c72ef6d126412176590dbe812b2adbb
c-ramos <carolinaram@me.com>
2022-06-07 18:45:00 -0400

    Final commit

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -18,3 +18,17 @@ total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI),Median=median(PSI)
 
 #write script that creates a lot_summary DF using groupby() and summarize()
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep')
+
+#Deliverable 3
+
+#t-test across all Lots
+t.test(Suspension_Coil$PSI,mu = 1500)
+
+# t-test on Lot 1
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+
+# t-test on Lot 2
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+
+# t-test on Lot 3
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
